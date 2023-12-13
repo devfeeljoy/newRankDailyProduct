af7a12bb84c4b4f727852b7c4cf28b54bf371bd4
woopil <jwp4465@highdev.pro>
2023-12-13 13:42:20 +0900

    feat: newRank api 테스트

--- hello-world/go.mod
+++ hello-world/go.mod
@@ -1,4 +1,8 @@
-require github.com/aws/aws-lambda-go v1.36.1
+require (
+	github.com/aws/aws-lambda-go v1.36.1
+	github.com/aws/aws-sdk-go v1.49.0
+	github.com/linkedin/goavro/v2 v2.12.0
+)
 
 replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 
--- hello-world/main.go
+++ hello-world/main.go
@@ -5,13 +5,94 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/linkedin/goavro/v2"
 )
+
+const avroSchema = `{
+ "type" : "record",
+ "name" : "BuridgeAccountInfoAvro",
+ "namespace" : "cn.newrank.api.dy.custom.project.system.biz.buridge.model.avro",
+ "fields" : [ {
+   "name" : "uid",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "account",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "nickname",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "introduction",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "avatar",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "gender",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "mcnName",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "enterpriseVerify",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "customVerify",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "dyFansNum",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "fansNum",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "likeNum",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "opusNum",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "homeUrl",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "newrankWeekIndex",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "age",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "mainCategory",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "ipLocation",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "location",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "firstCategory",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "secondCategory",
+   "type" : [ "string", "null" ]
+ }, {
+   "name" : "updateTime",
+   "type" : [ "string", "null" ]
+ } ]
+}`
 
 // API 응답 구조체
 type ApiResponse struct {
@@ -27,6 +108,125 @@ type DataItem struct {
 	Name string `json:"name"`
 }
 
+func downloadAvroFile(url string, filePath string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// 파일 생성
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 응답 본문을 파일에 쓰기
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func convertAndUploadToDynamoDB(filePath string) error {
+	//Avro 코덱 생성
+	codec, err := goavro.NewCodec(avroSchema)
+	if err != nil {
+		return err
+	}
+
+	// 파일 열기
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Avro 파일 열기
+	fr, err := goavro.NewOCFReader(file)
+	if err != nil {
+		return err
+	}
+
+	//DynamoDB 세션 및 클라이언트 초기화
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	dynamoDB := dynamodb.New(sess)
+
+	var writeRequests []*dynamodb.WriteRequest
+	// Avro 레코드를 읽고 DynamoDB에 저장
+	for fr.Scan() {
+		// 현재 레코드 읽기
+		raw, err := fr.Read()
+		if err != nil {
+			return err
+		}
+
+		// Raw 데이터를 바이너리로 변환
+		binary, err := codec.BinaryFromNative(nil, raw)
+		if err != nil {
+			return err
+		}
+
+		// Binary 데이터를 네이티브 Go 타입으로 역직렬화
+		native, _, err := codec.NativeFromBinary(binary)
+		if err != nil {
+			return err
+		}
+
+		// 필요한 데이터 변환 수행
+		record, ok := native.(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("record is not a map")
+		}
+
+		// 예를 들어, 'string' 타입의 필드를 처리
+		for key, value := range record {
+			if valueMap, ok := value.(map[string]interface{}); ok {
+				if stringValue, ok := valueMap["string"].(string); ok {
+					record[key] = stringValue
+				}
+			}
+		}
+
+		//DynamoDB 항목 매핑
+		item, err := dynamodbattribute.MarshalMap(record)
+		if err != nil {
+			return err
+		}
+
+		writeRequest := &dynamodb.WriteRequest{PutRequest: &dynamodb.PutRequest{Item: item}}
+		writeRequests = append(writeRequests, writeRequest)
+
+		if len(writeRequests) >= 25 {
+			err = batchWriteToDynamoDB(dynamoDB, "Influencer", writeRequests)
+			if err != nil {
+				return err
+			}
+			writeRequests = writeRequests[:0] // 슬라이스 초기화
+		}
+	}
+	// 마지막 배치 처리
+	if len(writeRequests) > 0 {
+		err = batchWriteToDynamoDB(dynamoDB, "Influencer", writeRequests)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func batchWriteToDynamoDB(dynamoDB *dynamodb.DynamoDB, tableName string, writeRequests []*dynamodb.WriteRequest) error {
+	batchInput := &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]*dynamodb.WriteRequest{
+			tableName: writeRequests,
+		},
+	}
+
+	_, err := dynamoDB.BatchWriteItem(batchInput)
+	return err
+}
+
 // Lambda 핸들러 함수
 func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	apiKey := os.Getenv("key") // 환경 변수에서 API 키 가져오기
@@ -39,7 +239,7 @@ func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) 
 	}
 
 	// 전날 날짜 계산
-	yesterday := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+	yesterday := time.Now().AddDate(0, 0, -2).Format("2006-01-02")
 
 	apiUrl, err := url.Parse("https://api.newrank.cn/api/v2/custom/common/buridge/file/list")
 	if err != nil {
@@ -49,7 +249,7 @@ func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) 
 	query := apiUrl.Query()
 	query.Set("date", yesterday)
 	apiUrl.RawQuery = query.Encode()
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: 300 * time.Second}
 	req, err := http.NewRequest("GET", apiUrl.String(), nil)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
@@ -72,8 +272,6 @@ func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) 
 		return events.APIGatewayProxyResponse{}, err
 	}
 
-	// 응답 데이터를 JSON 문자열로 변환
-	responseBody, err := json.Marshal(apiResp)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			Body:       err.Error(),
@@ -81,8 +279,43 @@ func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) 
 		}, nil
 	}
 
+	var downloadUrl, fileName string
+	for _, item := range apiResp.Data {
+		if strings.Contains(item.Name, "buridge_dy_account_daily_data") {
+			downloadUrl = item.URL
+			fileName = item.Name
+			break
+		}
+	}
+
+	if downloadUrl == "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "No matching data found",
+			StatusCode: 404,
+		}, nil
+	}
+
+	// 파일 경로 생성 (/tmp 폴더와 파일 이름 결합)
+	filePath := filepath.Join("/tmp", fileName)
+	// Avro 파일 다운로드
+	err = downloadAvroFile(downloadUrl, filePath)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("Failed to download Avro file: %s", err),
+			StatusCode: 500,
+		}, nil
+	}
+	err = convertAndUploadToDynamoDB(filePath)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       fmt.Sprintf("DB save Fail: %s", err),
+			StatusCode: 500,
+		}, nil
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       string(responseBody),
+		Body:       "완료",
 		StatusCode: 200,
 	}, nil
 }
