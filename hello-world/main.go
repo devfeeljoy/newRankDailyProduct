4b85277116eb9d6dafca415ad5fa3cd03bb3251d
woopil <jwp4465@highdev.pro>
2024-01-03 09:45:58 +0900

    feat: product Daily batch

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,9 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"io"
 	"net/http"
 	"net/url"
@@ -17,8 +17,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
-
-	"github.com/linkedin/goavro/v2"
 )
 
 // API 응답 구조체
@@ -35,7 +33,24 @@ type DataItem struct {
 	Name string `json:"name"`
 }
 
+const MaxRetries = 5
+
+func retryOperation(operation func() error) error {
+	var lastErr error
+	for i := 0; i < MaxRetries; i++ {
+		err := operation()
+		if err == nil {
+			return nil
+		}
+		lastErr = err
+		// 선택적으로 재시도 간 딜레이를 추가할 수 있습니다.
+		time.Sleep(time.Second * 1)
+	}
+	return fmt.Errorf("operation failed after %d attempts: %v", MaxRetries, lastErr)
+}
+
 func downloadAvroFile(url string, filePath string) error {
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
@@ -54,101 +69,37 @@ func downloadAvroFile(url string, filePath string) error {
 	return err
 }
 
-func convertAndUploadToMongoDB(filePath string, mongoClient *mongo.Client, databaseName string, collectionName string) error {
-	// 파일 열기
+func uploadToS3(filePath string, bucketName string, s3Key string) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-2"), // AWS 리전 설정
+	})
+	if err != nil {
+		return err
+	}
+
+	s3Client := s3.New(sess)
 	file, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	// Avro 파일 열기
-	fr, err := goavro.NewOCFReader(file)
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(s3Key),
+		Body:   file,
+	})
 	if err != nil {
 		return err
 	}
-
-	// MongoDB 컬렉션 선택
-	collection := mongoClient.Database(databaseName).Collection(collectionName)
-
-	var documents []interface{}
-	var currentBatchSize int
-	// Avro 레코드를 읽고 MongoDB에 저장
-	for fr.Scan() {
-		// 현재 레코드 읽기
-		raw, err := fr.Read()
-		if err != nil {
-			return err
-		}
-
-		// 필요한 데이터만 추출 (raw는 이미 map[string]interface{} 타입으로 역직렬화된 것으로 가정)
-		record, ok := raw.(map[string]interface{})
-		if !ok {
-			return fmt.Errorf("record is not a map")
-		}
-
-		// 필요한 데이터 변환 수행
-		for key, value := range record {
-			if valueMap, ok := value.(map[string]interface{}); ok {
-				if stringValue, ok := valueMap["string"].(string); ok {
-					record[key] = stringValue
-				}
-			}
-		}
-
-		// BSON으로 문서 변환 후 크기 추정
-		bsonData, err := bson.Marshal(record)
-		if err != nil {
-			return err
-		}
-		bsonSize := len(bsonData)
-
-		// 일정 크기에 도달하면 배치 삽입 실행
-		if currentBatchSize+bsonSize > 15*1024*1024 {
-			_, err = collection.InsertMany(context.Background(), documents)
-			if err != nil {
-				return err
-			}
-			documents = []interface{}{} // 슬라이스 초기화
-			currentBatchSize = 0
-		}
-		// MongoDB 문서로 변환
-		documents = append(documents, record)
-		currentBatchSize += bsonSize
-	}
-
-	// 남은 문서 처리
-	if len(documents) > 0 {
-		_, err = collection.InsertMany(context.Background(), documents)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return err
 }
 
 // Lambda 핸들러 함수
 func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) {
-	//mongoDB 연결
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://sparta:p8hDJNs18HANQ7eU@cluster0.5pjmefm.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
-	// Create a new client and connect to the server
-	clientMongo, err := mongo.Connect(context.TODO(), opts)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err = clientMongo.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-	// Send a ping to confirm a successful connection
-	if err := clientMongo.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-		panic(err)
-	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
 	apiKey := os.Getenv("key") // 환경 변수에서 API 키 가져오기
+	bucketName := os.Getenv("BUCKET_NAME")
 	if apiKey == "" {
 		fmt.Println("API key is not set in environment variables.")
 		return events.APIGatewayProxyResponse{
@@ -177,58 +128,47 @@ func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) 
 	// 헤더 설정
 	req.Header.Set("key", apiKey)
 
-	// API 요청
-	resp, err := client.Do(req)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-	defer resp.Body.Close()
-
-	// 응답 처리
-	var apiResp ApiResponse
-	err = json.NewDecoder(resp.Body).Decode(&apiResp)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			Body:       err.Error(),
-			StatusCode: 500,
-		}, nil
-	}
-
-	var downloadUrl, fileName string
-	for _, item := range apiResp.Data {
-		if strings.Contains(item.Name, "buridge_dy_product_daily_data") {
-			downloadUrl = item.URL
-			fileName = item.Name
-			break
+	err = retryOperation(func() error {
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("API 요청 실패: %w", err)
 		}
-	}
+		defer resp.Body.Close()
 
-	if downloadUrl == "" {
-		return events.APIGatewayProxyResponse{
-			Body:       "No matching data found",
-			StatusCode: 404,
-		}, nil
-	}
+		var apiResp ApiResponse
+		if err := json.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
+			return fmt.Errorf("응답 처리 실패: %w", err)
+		}
 
-	// 파일 경로 생성 (/tmp 폴더와 파일 이름 결합)
-	filePath := filepath.Join("/tmp", fileName)
-	// Avro 파일 다운로드
-	err = downloadAvroFile(downloadUrl, filePath)
+		var downloadUrl, fileName string
+		for _, item := range apiResp.Data {
+			if strings.Contains(item.Name, "buridge_dy_product_daily_data") {
+				downloadUrl = item.URL
+				fileName = item.Name
+				break
+			}
+		}
+		if downloadUrl == "" || fileName == "" {
+			return fmt.Errorf("유효한 파일 URL 또는 이름을 찾을 수 없음")
+		}
+
+		filePath := filepath.Join("/tmp", fileName)
+		if err := downloadAvroFile(downloadUrl, filePath); err != nil {
+			return fmt.Errorf("Avro 파일 다운로드 실패: %w", err)
+		}
+
+		s3Key := fmt.Sprintf("NewRank/buridge_dy_product_daily_data/%s", fileName)
+		if err := uploadToS3(filePath, bucketName, s3Key); err != nil {
+			return fmt.Errorf("S3 업로드 실패: %w", err)
+		}
+
+		return nil
+	})
+
 	if err != nil {
+		fmt.Printf("Error in operation: %v\n", err)
 		return events.APIGatewayProxyResponse{
-			Body:       fmt.Sprintf("Failed to download Avro file: %s", err),
-			StatusCode: 500,
-		}, nil
-	}
-	err = convertAndUploadToMongoDB(filePath, clientMongo, "NewRank", "product_daily")
-
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			Body:       fmt.Sprintf("DB save Fail: %s", err),
+			Body:       fmt.Sprintf("Failed to process: %s", err),
 			StatusCode: 500,
 		}, nil
 	}
